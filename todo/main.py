00ac8b6c598d40610dd55514f8a6d3d3cbcbc76f
Tonio <toha.futbolist@gmail.com>
2023-02-07 20:05:01 +1000

    Connected Database and Created Settings

---
+++ todo/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='todo/static'), name='static')
+templates = Jinja2Templates(directory='todo/templates')
