8ad35eb032cd214f5e761e9436c34d92a67d1409
Tonio <toha.futbolist@gmail.com>
2023-02-11 23:02:39 +1000

    Routes CRUD

--- todo/main.py
+++ todo/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from .routes import home
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='todo/static'), name='static')
